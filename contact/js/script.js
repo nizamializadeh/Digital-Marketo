66d4d2d74c06417fbaf68c2cfcdf7afec0a407d0
Nizami Alizade <nizamializadeh>
2017-08-06 14:18:51 +0400

    final template finsh

--- contact/js/script.js
+++ contact/js/script.js
@@ -1,3 +1,50 @@
+var condition=false;
+$(".fa-bars").click(function(){
+    if (condition==false) {
+        $(".mainDrop").slideDown()
+        condition=true;
+    }
+    else {
+        $(".mainDrop").slideUp()
+        condition=false;
+    }
+})
+// active click on navigation
+$(".navBarChoices ul li a").click(function(elem) {
+    $(".navBarChoices ul li a").removeClass("activateLink");
+    $(this).addClass("activateLink");
+})
+
+// event listener for services button
+var serCond=false
+var $services = $(".nav .navBar .navBarChoices > ul > li:nth-child(1)");
+var $serDropDown = $(".nav .navBar .navBarChoices > ul > li:nth-child(1) ul")
+$($services).click(function(){
+    if (serCond==false) {
+        $($serDropDown).slideDown()
+        serCond=true;
+    }
+    else {
+        $($serDropDown).slideUp()
+        serCond=false;
+    }
+})
+
+// event listener for services button in the main dropdown
+var mainSerCond=false
+var $mainServices = $(".mainDrop > ul > li:nth-child(1)");
+var $mainSerDropDown = $(".mainDrop > ul > li:nth-child(1) > ul")
+$($mainServices).click(function(){
+    if (mainSerCond==false) {
+        $($mainSerDropDown).slideDown()
+        mainSerCond=true;
+    }
+    else {
+        $($mainSerDropDown).slideUp()
+        mainSerCond=false;
+    }
+})
+
 var map1 = document.getElementsByClassName("maps1")[0];
 var map2 = document.getElementsByClassName("maps2")[0];
 var map3 = document.getElementsByClassName("maps3")[0];
